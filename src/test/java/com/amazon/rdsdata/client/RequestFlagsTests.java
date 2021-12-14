77ca078b114329a34b09a9621d4e313bc9a2b211
Sergey Polyakov <polyas@amazon.com>
2021-12-14 14:45:22 -0800

    Tests for continueAfterTimeout

---
+++ src/test/java/com/amazon/rdsdata/client/RequestFlagsTests.java
@@ -0,0 +1,34 @@
+package com.amazon.rdsdata.client;
+
+import com.amazon.rdsdata.client.testutil.TestBase;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RequestFlagsTests extends TestBase {
+  @BeforeEach
+  public void beforeEach() {
+    mockReturnValue(); // return empty response by default
+  }
+
+  @Test
+  public void shouldSetContinueAfterTimeoutFlag() {
+    client.forSql("SELECT 1")
+        .withContinueAfterTimeout()
+        .execute();
+
+    val request = captureRequest();
+    assertThat(request.getContinueAfterTimeout()).isTrue();
+  }
+
+  @Test
+  public void shouldNotSetContinueAfterTimeoutFlagByDefault() {
+    client.forSql("SELECT 1")
+        .execute();
+
+    val request = captureRequest();
+    assertThat(request.getContinueAfterTimeout()).isFalse();
+  }
+}
